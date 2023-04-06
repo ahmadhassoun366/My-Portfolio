7e7c52fd39e21c9e354cdd2c3bc92a21a9e5f073
ahmadhasssoun366 <ahmadhss366@gmail.com>
2023-04-06 05:15:50 +0300

    Adding Google Analytics Tag

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from 'react';
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
@@ -8,8 +8,15 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Bot from "./components/Bot";
+import ReactGA from 'react-ga';
 
 const App = () => {
+
+  useEffect(() => {
+    ReactGA.initialize('G-8L1EKGQM2T');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <div>
       <Navbar />
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-8L1EKGQM2T');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
